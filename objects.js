4ee1ac8817b08a38a8fb1b68d422f91db3fcb87e
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-03-30 17:56:46 -0300

    Add files via upload
    Creating and factoring objects

---
+++ objects.js
@@ -0,0 +1,29 @@
+// Factory Functions
+
+function createCircle(radius) {
+	return {
+		radius,
+		draw() {
+			console.log('draw');
+		}
+	};
+}
+
+const circle1 = createCircle(1);
+console.log(circle1);
+
+const circle2 = createCircle(2);
+console.log(circle2);
+
+//OLD ONE
+// const circle = {
+// 	radius: 1,
+// 	location: {
+// 		x: 1,
+// 		y: 1
+// 	},
+// 	isVisible: true,
+// 	draw: function() {
+// 		console.log('draw');
+// 	}
+// };
