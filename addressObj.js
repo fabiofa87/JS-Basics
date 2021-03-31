0a11679d1453284964bfa38215bb37ccd3352704
Fabio Faria <64399840+fabiofa87@users.noreply.github.com>
2021-03-31 17:07:38 -0300

    Update addressObj.js

--- addressObj.js
+++ addressObj.js
@@ -1,11 +1,37 @@
-const address = {
-	street: 'a',
-	city: 'Rio de Janeiro',
-	zipcode: 'a'
-};
+// const address = {
+// 	street: 'a',
+// 	city: 'Rio de Janeiro',
+// 	zipcode: 'a'
+// };
 
-showAddress(address);
+// showAddress(address);
 
-function showAddress(address) {
-	for (let key in address) console.log(key, address[key]);
+// function showAddress(address) {
+// 	for (let key in address) console.log(key, address[key]);
+// }
+
+// Constructor
+// function Address(street, city, zipcode) {
+// 	(this.street = street), (this.city = city), (this.zipcode = zipcode);
+// }
+
+// const address = new Address('a', 'b', 'c');
+
+// console.log(address);
+
+//Factory Function
+
+function createAddress(street, city, zipcode) {
+	return {
+		street,
+		city,
+		zipcode
+	};
 }
+
+// console.log(createAddress('a', 'b', 'c'));
+
+// Or
+
+const address = createAddress('a', 'b', 'c');
+console.log(address);
